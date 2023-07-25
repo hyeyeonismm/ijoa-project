a9f8791b1d3e2a81ad768e5c7cb769daa4f2266f
hyeyeonismm <hyeyeonismm@gmail.com>
2023-07-25 21:26:32 +0900

    Mainpage setting

---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Main page</h1>
+    </div>
+  );
+}
+
+export default MainPage;
