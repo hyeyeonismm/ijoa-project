470eaa069473b9819a4179db08de18cac501a785
goraniiii <gony06011@gmail.com>
2023-08-02 15:40:24 +0900

    Edit LoginPage

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,12 +1,39 @@
-import Header from "../components/Header";
+
+
 
 function LoginPage() {
   return (
     <>
-      <Header />
-      <div>
-        <h1>Login page</h1>
-      </div>
+      <div style={{width: '100%', height: '100%', background: 'rgba(93, 90, 136, 0.70)'}}></div>
+      
+      
+      <div style={{width: '100%', height: '100%', position: 'relative'}}>
+    <div style={{width: 539, height: 741, left: 0, top: 0, position: 'absolute', background: 'white', boxShadow: '0px 4px 35px rgba(0, 0, 0, 0.08)', borderRadius: 40}} />
+    <div style={{width: 124, left: 404, top: 134, position: 'absolute'}}><span style={{color: '#8D8D8D', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>계정이 없나요?<br/></span><span style={{color: '#B87514', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>회원가입하기</span></div>
+    <div style={{width: 451, height: 92, left: 44, top: 202, position: 'absolute'}}>
+        <div style={{left: 0, top: -2, position: 'absolute', color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>아이디를 입력하세요</div>
+        <div style={{width: 451, height: 57, left: 0, top: 35, position: 'absolute', background: 'white', borderRadius: 8, border: '0.50px #C0C0C0 solid'}} />
+        <div style={{width: 302, height: 19, left: 25, top: 54, position: 'absolute', color: '#ABABAB', fontSize: 18, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>아이디</div>
+    </div>
+    <div style={{width: 451, height: 123, left: 44, top: 332, position: 'absolute'}}>
+        <div style={{width: 451, height: 92, left: 0, top: 0, position: 'absolute'}}>
+            <div style={{left: 0, top: -2, position: 'absolute', color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호를 입력하세요</div>
+            <div style={{width: 451, height: 57, left: 0, top: 35, position: 'absolute', background: 'white', borderRadius: 8, border: '0.50px #C0C0C0 solid'}} />
+            <div style={{width: 302, height: 19, left: 25, top: 54, position: 'absolute', color: '#ABABAB', fontSize: 18, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호</div>
+        </div>
+        <div style={{left: 365, top: 105, position: 'absolute', color: 'rgba(173, 49, 19, 0.80)', fontSize: 14, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호 찾기</div>
+    </div>
+    <div style={{width: 451, height: 474, left: 44, top: 71, position: 'absolute'}}>
+        <div style={{width: 22, height: 19, left: 410, top: 301, position: 'absolute'}}>
+            <div style={{width: 22, height: 12.11, left: 0, top: 3.45, position: 'absolute', background: '#8E8E8E'}}></div>
+            <div style={{width: 8.70, height: 7.52, left: 6.65, top: 5.74, position: 'absolute', background: '#8E8E8E'}}></div>
+        </div>
+        <div style={{width: 451, height: 54, left: 0, top: 420, position: 'absolute', background: '#5D5A88', boxShadow: '0px 4px 19px rgba(119, 147, 65, 0.30)', borderRadius: 10}} />
+        <div style={{width: 100, left: 180, top: 434, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'DM Sans', fontWeight: '700', wordWrap: 'break-word'}}>로그인하기</div>
+        <div style={{left: 176.34, top: 0, position: 'absolute', textAlign: 'center', color: '#5D5A88', fontSize: 36, fontFamily: 'DM Sans', fontWeight: '700', lineHeight: 46, wordWrap: 'break-word'}}>로그인</div>
+    </div>
+</div>
+      
     </>
   );
 }
