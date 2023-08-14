34a35f1d5f19de5c5d3e64f24a6185ee3e5fee82
goraniiii <gony06011@gmail.com>
2023-08-14 17:59:54 +0900

    edit LoginPage

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -8,49 +8,22 @@ import { alignProperty } from "@mui/material/styles/cssUtils";
 function LoginPage() {
   return (
     <>
-      <Container>
-        <div style={{width: 539, height: 741, left: 0, top: 0, position: 'absolute', background: 'white', boxShadow: '0px 4px 35px rgba(0, 0, 0, 0.08)', borderRadius: 40}}> 
-          <div style={{textAlign: 'center', color: '#5D5A88', fontSize: 36, fontFamily: 'DM Sans', fontWeight: '700', lineHeight: 46}}>로그인</div>
-
-          <div style={{width: 124, left: 404, top: 134, position: 'relative'}}>
-            <span style={{color: '#8D8D8D', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>계정이 없나요?<br/></span><span style={{color: '#B87514', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>회원가입하기</span>
+      <LoginBox>
+        <div style={{textAlign: 'center', color: '#5D5A88', fontSize: 36, fontFamily: 'DM Sans', fontWeight: '700', lineHeight: 5}}>로그인</div>
+          <div style={{width: 450, position: 'relative'}}>
+            <div style={{color: '#8D8D8D', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', float: 'right'}}>계정이 없나요?</div><br/>
+            <div style={{color: '#B87514', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '400', float: 'right'}}>회원가입하기</div>
           </div>
           <div style={{width: 451, height: 92, position: 'relative'}}>
             <div style={{left: 0, top: -2, color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>아이디를 입력하세요</div>
-            <TextField width="451px" height="57px" label="아이디"/>
+            <IDField label="아이디"/>
           </div>
           <div style={{width: 451, height: 92, position: 'relative'}}>
-            <div style={{left: 0, top: -2, color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호를 입력하세요</div>
-            <TextField width="451px" height="57px" label="비밀번호"/>
+            <div style={{left: 0, top: -2, color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400'}}>비밀번호를 입력하세요</div>
+            <PWField label="비밀번호"/>
           </div>
           <Login>로그인하기</Login>
-        </div>
-          
-      
-      </Container>
-      
-      
-      
-    
-    
-    
-    <div style={{width: 451, height: 123, left: 44, top: 332, position: 'absolute'}}>
-        <div style={{width: 451, height: 92, left: 0, top: 0, position: 'absolute'}}>
-            <div style={{left: 0, top: -2, position: 'absolute', color: '#8D8BA7', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호를 입력하세요</div>
-            <div style={{width: 451, height: 57, left: 0, top: 35, position: 'absolute', background: 'white', borderRadius: 8, border: '0.50px #C0C0C0 solid'}} />
-            <div style={{width: 302, height: 19, left: 25, top: 54, position: 'absolute', color: '#ABABAB', fontSize: 18, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호</div>
-        </div>
-        <div style={{left: 365, top: 105, position: 'absolute', color: 'rgba(173, 49, 19, 0.80)', fontSize: 14, fontFamily: 'DM Sans', fontWeight: '400', wordWrap: 'break-word'}}>비밀번호 찾기</div>
-    </div>
-    <div style={{width: 451, height: 474, left: 44, top: 71, position: 'absolute'}}>
-        <div style={{width: 22, height: 19, left: 410, top: 301, position: 'absolute'}}>
-            <div style={{width: 22, height: 12.11, left: 0, top: 3.45, position: 'absolute', background: '#8E8E8E'}}></div>
-            <div style={{width: 8.70, height: 7.52, left: 6.65, top: 5.74, position: 'absolute', background: '#8E8E8E'}}></div>
-        </div>
-        <div style={{width: 451, height: 54, left: 0, top: 420, position: 'absolute', background: '#5D5A88', boxShadow: '0px 4px 19px rgba(119, 147, 65, 0.30)', borderRadius: 10}} />
-        <div style={{width: 100, left: 180, top: 434, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'DM Sans', fontWeight: '700', wordWrap: 'break-word'}}>로그인하기</div>
-        
-    </div>
+      </LoginBox>
       
     </>
   );
@@ -68,6 +41,30 @@ const Login = styled(Button)(() => ({
   "&:hover": {
     background: "#5D5A88",
   },
+  float: 'center',
 }));
+
+const LoginBox = styled(Container)(() => ({
+  width: 539,
+  height: 741,
+  background: 'white',
+  boxShadow: '0px 4px 35px rgba(0, 0, 0, 0.08)',
+  borderRadius: 40,
+
+}));
+
+const IDField = styled(TextField)(() => ({
+  width: 450,
+  height: 55,
+}));
+
+const PWField = styled(TextField)(() => ({
+  width: 450,
+  height: 55,
+}));
+
+const onClickSignup = () => {
+  //useNavigate("/signup");
+}
 
 export default LoginPage;
